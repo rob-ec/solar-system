1c5844889daa7c99eae0200962728a17304a75d1
Yara <yrbraga@alu.ufc.br>
2023-10-13 22:30:28 -0300

    Adiciona controle de velocidade do movimento dos planetas pelo teclado e exibe informações na tela

--- Sistema_Solar/main.js
+++ Sistema_Solar/main.js
@@ -1,5 +1,5 @@
 import * as THREE from 'three';
-import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import starsTexture from './src/img/Atmosferas/stars.jpg';
 import sunTexture from './src/img/2k_sun.jpg';
@@ -14,7 +14,10 @@ import neptuneTexture from './src/img/2k_neptune.jpg';
 
 import saturnRingTexture from './src/img/Atmosferas/saturn ring.png';
 
-//Renderizador 
+// Velocidade inicial dos planetas (normal)
+let speed = 1;
+
+//Renderizador
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 
@@ -26,7 +29,7 @@ const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
   45,
-  window.innerWidth/window.innerHeight,
+  window.innerWidth / window.innerHeight,
   0.1,
   2000
 );
@@ -43,136 +46,156 @@ scene.background = cubeTextureLoader.load([
   starsTexture
 ]);
 
-//Colocando o Controle da orbita
+// Colocando o Controle da órbita
 
 const orbit = new OrbitControls(camera, renderer.domElement);
-camera.position.set(-90,140,140);
+camera.position.set(-90, 140, 140);
 
 // controle do estado da câmera
-orbit.saveState(); // salva o estado incial
-window.addEventListener('keydown', function(e){
+orbit.saveState(); // salva o estado inicial
+window.addEventListener('keydown', function (e) {
   if (e.code === 'KeyR')
     orbit.reset(); // reseta para o estado inicial
-})
+
+  // Ajusta a velocidade com base nas teclas '+' e '-'
+  if (e.code === 'Equal') { // Tecla '+'
+    speed *= 1.2;
+  } else if (e.code === 'Minus') { // Tecla '-'
+    speed /= 1.2;
+  }
+});
 orbit.enableDamping = true; // "peso" para o movimento da câmera
 
 orbit.update();
 
-//Inializando a luz no sistema
+// Inicializando a luz no sistema
 const ambientLight = new THREE.AmbientLight(0xFFFFFF);
 scene.add(ambientLight);
 
-//inicializando os planetas
+// Inicializando os planetas
 const textureLoad = new THREE.TextureLoader();
 
-//Adicionando o Sol
-const sunGeo = new THREE.SphereGeometry(40,20);
+// Adicionando o Sol
+const sunGeo = new THREE.SphereGeometry(40, 20);
 const sunMat = new THREE.MeshBasicMaterial({
-  map:textureLoad.load(sunTexture)
+  map: textureLoad.load(sunTexture)
 });
-const sun  = new THREE.Mesh(sunGeo,sunMat);
+const sun = new THREE.Mesh(sunGeo, sunMat);
 scene.add(sun);
 
-//adicionando uma luz direcional
-const light = new THREE.PointLight( 0xff0000, 1, 1000 );
-light.position.set( 0, 0, 0 );
-scene.add( light );
+// Adicionando uma luz direcional
+const light = new THREE.PointLight(0xff0000, 1, 1000);
+light.position.set(0, 0, 0);
+scene.add(light);
 
-//Função para inicializar outros planetas 
-function createPlanet(size,texture,position,ring){
-  const geometry = new THREE.SphereGeometry(size,25,20);
+// Função para inicializar outros planetas 
+function createPlanet(size, texture, position, ring) {
+  const geometry = new THREE.SphereGeometry(size, 25, 20);
   const material = new THREE.MeshStandardMaterial({
-    map:textureLoad.load(texture)
+    map: textureLoad.load(texture)
   });
-  const planet = new THREE.Mesh(geometry,material);
+  const planet = new THREE.Mesh(geometry, material);
   const planetObj = new THREE.Object3D;
   planetObj.add(planet);
   scene.add(planetObj);
-  planet.position.x = position
+  planet.position.x = position;
 
-  if(ring){
+  if (ring) {
     const RingGeo = new THREE.RingGeometry(
       ring.innerRadius,
       ring.outerRadius, 30
     );
     const RingMat = new THREE.MeshStandardMaterial({
-      map:textureLoad.load(ring.texture),
-      side : THREE.DoubleSide
+      map: textureLoad.load(ring.texture),
+      side: THREE.DoubleSide
     });
-    const Ring = new THREE.Mesh(RingGeo,RingMat);
+    const Ring = new THREE.Mesh(RingGeo, RingMat);
     planetObj.add(Ring);
 
     Ring.position.x = position;
     Ring.rotation.x = -0.5 * Math.PI;
   }
-  return {planet,planetObj};
+  return { planet, planetObj };
 }
 
-//Tamanho dos plaentas em escala com a terra
+// Tamanho dos planetas em escala com a Terra
 
-//Mercurio
-const mercury = new createPlanet(3,mercuryTexture,70);
+// Mercúrio
+const mercury = createPlanet(3, mercuryTexture, 70);
 
-//Vênus
-const venus = new createPlanet(4.6, venusTexture,120)
+// Vênus
+const venus = createPlanet(4.6, venusTexture, 120)
 
-//Terra
-const earth = new createPlanet(6, earthTexture, 220)
+// Terra
+const earth = createPlanet(6, earthTexture, 220)
 
-//Mars
-const mars = new createPlanet(7.8, marsTexture, 300)
+// Marte
+const mars = createPlanet(7.8, marsTexture, 300)
 
-//Jupiter
-const jupiter = new createPlanet(20,jupiterTexture,410)
+// Júpiter
+const jupiter = createPlanet(20, jupiterTexture, 410)
 
-//Saturno (contém anéis)
-const saturn = new createPlanet(15,saturnTexture,500,{
-  innerRadius:10,
-  outerRadius:20,
+// Saturno (contém anéis)
+const saturn = createPlanet(15, saturnTexture, 500, {
+  innerRadius: 10,
+  outerRadius: 20,
   texture: saturnRingTexture
 })
 
-//Urano 
-const uranus = new createPlanet(12,uranusTexture,700)
+// Urano 
+const uranus = createPlanet(12, uranusTexture, 700)
 
-//netuno
-const neptune = new createPlanet(10,neptuneTexture,800)
+// Netuno
+const neptune = createPlanet(10, neptuneTexture, 800)
 
-function animate(){
+// Elemento para mostrar a velocidade
+const speedInfo = document.createElement('div');
+speedInfo.style.position = 'absolute';
+speedInfo.style.bottom = '20px';
+speedInfo.style.right = '20px';
+speedInfo.style.color = 'white';
+document.body.appendChild(speedInfo);
+
+// Função de animação
+function animate() {
   sun.rotateY(0.002);
 
-  mercury.planet.rotateY(0.174616); // Rotação dos planetas em torno de seu eixo (rotação diária)
-  mercury.planetObj.rotateY(0.0203); // Órbita dos planetas ao redor do Sol (tempo para uma órbita completa)
+  mercury.planet.rotateY(0.174616 * speed);
+  mercury.planetObj.rotateY(0.0203 * speed);
 
+  venus.planet.rotateY(0.048611 * speed);
+  venus.planetObj.rotateY(0.008 * speed);
 
-  venus.planet.rotateY(0.048611);
-  venus.planetObj.rotateY(0.008);
-  
-  earth.planet.rotateY(0.007272);
-  earth.planetObj.rotateY(0.0049);
-  
-  mars.planet.rotateY(0.004107);
-  mars.planetObj.rotateY(0.0026);
+  earth.planet.rotateY(0.007272 * speed);
+  earth.planetObj.rotateY(0.0049 * speed);
 
-  jupiter.planet.rotateY(0.000434);
-  jupiter.planetObj.rotateY(0.0004);
-  
-  saturn.planet.rotateY(0.000465);
-  saturn.planetObj.rotateY(0.0002);
-  
-  uranus.planet.rotateY(0.000648);
-  uranus.planetObj.rotateY(0.0001);
-  
-  neptune.planet.rotateY(0.000538);
-  neptune.planetObj.rotateY(0.0001);
+  mars.planet.rotateY(0.004107 * speed);
+  mars.planetObj.rotateY(0.0026 * speed);
 
-  renderer.render(scene,camera);
+  jupiter.planet.rotateY(0.000434 * speed);
+  jupiter.planetObj.rotateY(0.0004 * speed);
+
+  saturn.planet.rotateY(0.000465 * speed);
+  saturn.planetObj.rotateY(0.0002 * speed);
+
+  uranus.planet.rotateY(0.000648 * speed);
+  uranus.planetObj.rotateY(0.0001 * speed);
+
+  neptune.planet.rotateY(0.000538 * speed);
+  neptune.planetObj.rotateY(0.0001 * speed);
+
+  // Info para controle de velocidade do movimento dos planetas
+  speedInfo.innerText = `Tecla ( + ): aumenta a velocidade de movimento dos planetas \n Tecla ( - ): diminui a velocidade de movimento dos planetas`;
+
+  renderer.render(scene, camera);
 }
 
+// Configura a função de animação para ser chamada a cada quadro
 renderer.setAnimationLoop(animate);
 
-window.addEventListener('resize', function(){
-  camera.aspect = window.innerWidth/window.innerHeight;
+// Atualiza o tamanho da janela ao redimensionar
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth,window.innerHeight);
-})
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
